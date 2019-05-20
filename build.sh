c067e4131d7f45d707179be2e4ac7b8e55d60dc6
Crypto Zealot <34049724+cryptozealot@users.noreply.github.com>
2019-05-20 17:43:40 +0300

    Updated to use variable URLs for latest releases
    Updated to use variable URLs for latest releases and script will be executed in CSGO folder for convenience, instead of copying directories.

--- build.sh
+++ build.sh
@@ -1,13 +1,31 @@
-mkdir csbuild
-cd csbuild
-wget https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git970-linux.tar.gz
-wget https://sm.alliedmods.net/smdrop/1.9/sourcemod-1.9.0-git6281-linux.tar.gz
-wget https://github.com/splewis/csgo-practice-mode/releases/download/1.3.3/practicemode_1.3.3.zip
-tar -zxvf mmsource-1.10.7-git970-linux.tar.gz
-tar -zxvf sourcemod-1.9.0-git6281-linux.tar.gz
-unzip practicemode_1.3.3.zip
-cp /home/ubuntu/admins_simple.ini /home/ubuntu/steam/csbuild/addons/sourcemod/configs/admins_simple.ini
-rm /home/ubuntu/steam/csbuild/mmsource-*
-rm /home/ubuntu/steam/csbuild/sourcemod-*
-mkdir /home/ubuntu/steam/csgoserv/csgo/addons
-cp -r /home/ubuntu/steam/csbuild/* /home/ubuntu/steam/csgoserv/csgo/
+# Automatic installation of the latest SourceMod, MetamodSource and Practise Mode Plugin releases for CS:GO server on Linux. 
+# HOW TO:
+# Copy this script to your CS:GO server folder - The same folder where the addons and cfg folder should be created.
+# Example: /home/steam/csgoserver/csgo/ - (The folder containing bin, cfg, maps, materials, models etc.)
+# Copy the script to the folder and run it  ./build.sh
+
+
+# Get latest release filenames
+SMURL=$(curl https://sm.alliedmods.net/smdrop/1.9/sourcemod-latest-linux)
+MMURL=$(curl https://mms.alliedmods.net/mmsdrop/1.10/mmsource-latest-linux)
+PRACURL=$(curl "https://github.com/splewis/csgo-practice-mode/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#')
+
+# Download latest builds
+wget https://mms.alliedmods.net/mmsdrop/1.10/$MMURL
+wget https://sm.alliedmods.net/smdrop/1.9/$SMURL
+wget https://github.com/splewis/csgo-practice-mode/releases/download/1.3.3/practicemode_$PRACURL.zip
+
+# Untar and unzip archives
+tar -zxvf $MMURL
+tar -zxvf $SMURL
+unzip practicemode_$PRACURL.zip
+
+# Cleanup
+rm $MMURL
+rm $SMURL
+rm practicemode_$PRACURL.zip
+rm README.md
+rm LICENSE
+
+# Copy admin config, do other configurations here.
+#cp /home/ubuntu/admins_simple.ini /home/ubuntu/steam/csgoserv/csgo/addons/sourcemod/config/
