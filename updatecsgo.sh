11247b8506ac821ff98315800e30d604fb135cc1
Georgi H <34049724+cryptozealot@users.noreply.github.com>
2019-07-13 18:03:53 +0300

    Update updatecsgo.sh

--- updatecsgo.sh
+++ updatecsgo.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+pkill srcds_linux
 pkill screen
-sleep 5
+
 cd /home/ubuntu/steam/
 ./steamcmd.sh +login anonymous +force_install_dir /home/ubuntu/steam/csgoserv/ +app_update 740 validate +quit
+
 cd /home/ubuntu/steam/csgoserv/
 ./mainstart.sh
